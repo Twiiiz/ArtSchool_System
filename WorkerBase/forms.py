eef388779bf1786c2fbc0454a30f1c7082f30e0b
Twiiiz <tymur.mustaev@gmail.com>
2023-12-11 17:37:47 +0200

    added base of page for adding student grade (without adding to database)

--- WorkerBase/forms.py
+++ WorkerBase/forms.py
@@ -4,9 +4,22 @@ class LoginForm(forms.Form):
    login = forms.CharField(max_length=20, required=True, label='Логін', widget=forms.TextInput(attrs={'class': 'username'}))
    password = forms.CharField(max_length=20, required=True, label='Пароль', widget=forms.PasswordInput(attrs={'class': 'password'}))
 
-class StudentForm(forms.Form):
-   first_name = forms.CharField(max_length=50, required=True, label="Ім'я")
-   last_name = forms.CharField(max_length=50, required=True, label="Прізвище")
-   patronymic = forms.CharField(max_length=50, label="По-батькові")
-   date_of_birth = forms.DateField(label='Дата народження')
-   
+# class StudentForm(forms.Form):
+#    first_name = forms.CharField(max_length=50, required=True, label="Ім'я")
+#    last_name = forms.CharField(max_length=50, required=True, label="Прізвище")
+#    patronymic = forms.CharField(max_length=50, label="По-батькові")
+#    date_of_birth = forms.DateField(label='Дата народження')
+
+# class TeacherForm(forms.Form):
+#       first_name = forms.CharField(max_length=50, required=True, label="Ім'я")
+#       last_name = forms.CharField(max_length=50, required=True, label="Прізвище")
+#       patronymic = forms.CharField(max_length=50, label="По-батькові")
+
+#       login = forms.CharField(max_length=20, required=True, label='Логін', widget=forms.TextInput(attrs={'class': 'username'}))
+#       password = forms.CharField(max_length=20, required=True, label='Пароль', widget=forms.PasswordInput(attrs={'class': 'password'}))
+
+class StudentGradeForm(forms.Form):
+   student = forms.ChoiceField(label='Учень', required=True)
+   grade = forms.IntegerField(min_value=1, max_value=5, required=True, label='Оцінка')
+   eval_elt = forms.ChoiceField(label='Оцінюваний елемент', required=True)
+   lesson = forms.ChoiceField(label='Проведене Заняття', required=True)
--- WorkerBase/urls.py
+++ WorkerBase/urls.py
@@ -4,14 +4,17 @@ from .views import *
 urlpatterns = [
     path('', handleLogin),
     path('coord_page/', showCoordPage),
-    path('teacher_page/', showTeacherPage),
+    path('teacher_page/', showTeacherPage, name='show-teacher-page'),
     path('teacher_page/lessons/', showTeacherLessonsPage, name='show-teacher-lesson-page'),
     path('teacher_page/class/<int:class_id>/', include([
       path('', showClassPage, name='show-teacher-class-page'),
-      path('student_stat/', showStudentStatsPage, name='show-teacher-student-stats-page'),
+      path('student_stat/', include([
+        path('', showStudentStatsPage, name='show-teacher-student-stats-page'),
+        path('add_grade/', addStudentGrade)
+      ])),
       path('<int:student_id>/', showStudentPage, name='show-teacher-student-page'),
       path('student_comp/', showStudentCompPage, name='show-teacher-student-comp-page')
     ])),
     path('coord_page/view_teachers/', showTeachersPage),
-    path('coord_page/view_classes/', showClassesPage)
+    path('coord_page/view_classes/', showClassesPage),
 ]
